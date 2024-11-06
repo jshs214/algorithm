bb213baca16b6588afe740dfe719853d5b4425aa
labtob <jshs214@naver.com>
2024-11-06 09:41:55 +0900

    dp

--- 17258.cpp
+++ 17258.cpp
@@ -3,54 +3,65 @@ using namespace std;
 
 int n, m, k, t, a, b;
 
+// 시간 별 사람 count 배열
 int cnt[301];
+// use시간, 사람 수
+vector<pair<int, int>> v;
+// 구간v인덱스, 영선이 친구
 int dp[301][301];
 
-vector<pair<int, int>> v;
-
-// 인덱스, 영선이 남은 친구, 전에 쓴 영선이 친구
+// 인덱스, 영선 친구, 이전에 사용
 int go(int here, int num, int prev) {
 	if (here == v.size()) return 0;
-	if (dp[here][num]) return dp[here][num];
-
-	int cost = max(0, t - v[here].second);
-	int real_cost = (prev >= cost) ? 0 : cost - prev;
 
 	int& ret = dp[here][num];
-	//if (cost == 0) prev = 0;
+	if (ret) return ret;
+
+	// 시간 별 필요한 영선이 친구 수
+	int cost = max(0, t - v[here].second);
+	// 현재 구간에 필요한 영선이 친구 수(이전 반영)
+	int real_cost = (prev >= cost) ? 0 : cost - prev;
+
+	ret = dp[here][num];
+	if (cost == 0) prev = 0;
+
+	//영선이 친구로 가능하면
 	if (num - real_cost >= 0) {
 		ret = max(go(here + 1, num - real_cost, cost) + v[here].first,
-			go(here + 1, num, prev));
+			go(here + 1, num, prev)) ;
 	}
-	else ret = go(here + 1, num, prev);
-
-
+	// 불가 하면
+	else
+		ret = go(here + 1, num, 0);
 	return ret;
+
 }
+
 int main() {
 	cin >> n >> m >> k >> t;
 
-	for (int j = 0; j < m; j++) {
+	for (int i = 0; i < m; i++) {
 		cin >> a >> b;
-		for (int i = a; i < b; i++)
-			cnt[i] = min(t, ++cnt[i]);
+		for (int j = a; j < b; j++) {
+			cnt[j] = min(t, cnt[j] +1);
+		}
 	}
 
-
 	int temp = cnt[1];
-	int _count = 1;		//구간 길이
+	int count = 1;
 
 	for (int i = 2; i <= n; i++) {
 		if (temp != cnt[i]) {
-			v.push_back({ _count, temp });
-			_count = 0;
+			v.push_back({ count, temp });
+			count = 0;
 			temp = cnt[i];
 		}
-		_count++;
+		count++;
 	}
-	v.push_back({ _count, temp });	// 마지막 구간
+	v.push_back({ count, temp });
 
 	cout << go(0, k, 0);
+
 
 
 	return 0;
