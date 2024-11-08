672c52085c1aa82c1bdafcdfd86965834cd4867b
labtob <jshs214@naver.com>
2024-11-08 15:52:12 +0900

    플로이드

--- 11658.cpp
+++ 11658.cpp
@@ -27,16 +27,15 @@ void update(int y, int x, int value) {
 		}
 		y += (y & -y);
 	}
-	return;
 }
+
 
 int main() {
 	cin >> n >> m;
-	
+
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= n; j++) {
 			cin >> a[i][j];
-			// y, x
 			update(i, j, a[i][j]);
 		}
 	}
@@ -44,22 +43,22 @@ int main() {
 	while (m--) {
 		int w;
 		cin >> w;
-		// x,y를 c로 바꾸기
+
 		if (w == 0) {
-			int x, y, c;
+			int y, x, c;
 			cin >> x >> y >> c;
 
 			int diff = c - a[y][x];
 			a[y][x] = c;
 			update(y, x, diff);
 		}
-		// x1,y1 ~ x2,y2 합
 		else {
-			int x1, y1, x2, y2;
+			int y1, x1, y2, x2;
 			cin >> x1 >> y1 >> x2 >> y2;
 
-			cout << sum(y2, x2) - sum(y2, x1 -1) - sum(y1 -1, x2) + sum(y1 -1, x1-1) <<"\n";
+			cout << sum(y2, x2) - sum(y1 - 1, x2) - sum(y2, x1 - 1) + sum(y1 - 1, x1 - 1) <<'\n';
 		}
+
 	}
 
 	return 0;
---
+++ 1486.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int INF = 987654321;
+
+int dist[30][30];
+vector<int> v;		//1Â÷¿ø
+
+int cost[3000][3000];
+
+int dy[] = { -1,1,0,0 };
+int dx[] = { 0,0, -1,1 };
+
+int ret;
+int n, m, t, d;
+string s;
+int main() {
+	cin >> n >> m >> t >> d;
+
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		for (int j = 0; j < m; j++) {
+			if (s[j] >= 'a' && s[j] <= 'z') {
+				dist[i][j] = s[j] - 'a' + 26;
+			}
+			else dist[i][j] = s[j] - 'A';
+
+			v.push_back(i * 100 + j);
+		}
+	}
+	ret = dist[0][0];
+	fill(&cost[0][0], &cost[0][0] + 3000 * 3000, INF);
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			for (int d = 0; d < 4; d++) {
+				int ny = i + dy[d];
+				int nx = j + dx[d];
+
+				if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+				
+				int height_diff = abs(dist[i][j] - dist[ny][nx]);
+				if (height_diff <= t) {
+					if (dist[ny][nx] > dist[i][j]) {
+						cost[i * 100 + j][ny * 100 + nx] = height_diff * height_diff;
+					}
+					else
+						cost[i * 100 + j][ny * 100 + nx] = 1;
+				}
+
+			}
+		}
+	}
+
+	for (int k : v) {
+		for (int i : v) {
+			for (int j : v) {
+				cost[i][j] = min(cost[i][j], cost[i][k] + cost[k][j]);
+			}
+		}
+	}
+
+	for (int i : v) {
+		if (d >= cost[0][i] + cost[i][0]) {
+			ret = max(ret, dist[i / 100][i % 100]);
+		}
+	}
+
+	cout << ret;
+
+
+
+
+
+	return 0;
+}
