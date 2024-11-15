758d5f87547f4ecc348cd091e2990af0542ec371
Sung Ju Hong / 홍성주 <84310301+jshs214@users.noreply.github.com>
2024-11-15 15:45:31 +0900

    [level 3] Title: 등굣길, Time: 0.11 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.cpp
+++ 프로그래머스/3/42898. 등굣길/등굣길.cpp
@@ -34,9 +34,12 @@ int solve(int y, int x){
 }
 
 int solution(int m, int n, vector<vector<int>> puddles) {
+    int answer = 0;
+    
+    // top-down
+    #if 0
     N = n;
     M = m;
-    int answer = 0;
     
     for(int i =1; i <=n; i ++){
         for(int j = 1; j <=m; j++){
@@ -54,6 +57,37 @@ int solution(int m, int n, vector<vector<int>> puddles) {
     fill(&dp[0][0] , &dp[0][0]  + 105 * 105, -1);
     
     answer = solve(1, 1);
+    #endif
+    //
     
-    return answer;
+    
+    // bottom-up
+    for(int i =1; i <=n; i ++){
+        for(int j = 1; j <=m; j++){
+            cin >>a[i][j];
+        }
+    }
+    for (vector<int> puddle : puddles) {
+        int x = puddle[0]; 
+        int y = puddle[1]; 
+        dp[y][x] = -1;
+    }
+    
+    dp[1][1] =1;
+    for(int i = 1; i <=n; i++){
+        for(int j = 1; j <=m; j++){
+            if(dp[i][j] == -1) continue;
+            int a= 0;
+            int b= 0;
+            if(dp[i-1][j] != -1)
+                a = dp[i-1][j];
+            if(dp[i][j-1] != -1)
+                b = dp[i][j-1];
+            
+            dp[i][j] +=(a+b)%mod;
+        }
+    }
+    
+    
+    return dp[n][m];
 }
