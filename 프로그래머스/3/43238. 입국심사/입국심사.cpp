e2af74f2b6d89e1687bf874b66c196f99def98c3
Sung Ju Hong / 홍성주 <84310301+jshs214@users.noreply.github.com>
2024-11-19 15:10:25 +0900

    [level 3] Title: 입국심사, Time: 53.06 ms, Memory: 7.06 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -1,40 +1,46 @@
-#include <algorithm>
+#include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+typedef long long ll;
+vector<int> v;
+int N;
+
+bool check(ll T){
+    
+    ll ans = 0;
+    for(int i = 0; i < v.size() ; i ++){
+        ans +=  (T / (ll)v[i] );
+    }
+    
+    return (ans >= N) ? true: false;
+}
+
 long long solution(int n, vector<int> times) {
     long long answer = 0;
-	// 가장 큰 값을 뽑기 위해 정렬
-	// 해당 함수는 max_element()를 사용해도 됨.
+    
+    N = n;
     sort(times.begin(), times.end());
-
-    long long min = 1;
-
-	// 주의. 데이터타입
-    long long max = n * (long long)times.back();
-
-	/* 이분탐색 */
-
-	// 최대값과 최소값이 바뀌는 구간이 가장 최소 시간이다.
-    while (min <= max) {
-
-        long long avg = (max + min) / 2;
-        long long tmp = 0;
-
-			// 현재 시간 기준으로 심사위원들이
-			// 몇명을 처리하는지 확인하는 부분
-        for (int i = 0; i < times.size(); i++) {
-            tmp += (avg / (long long) times[i]);
-        }
-			// 현재 값 보다 많은 사람을 처리할 수 있을 경우
-        if (tmp >= n) {
-            max = avg - 1;
-            answer = avg;
-        }
-			// 현재 값보다 적은 사람을 처리할 수 있을 경우
-        else min = avg + 1;
+    v = times;
+    
+    ll l = 1;
+    ll r = (ll)n * v[v.size()-1];
+    
+    while(l <= r){
+        ll mid = (l + r) /2;
+        
+        if(check(mid) ){
+            r = mid -1;
+            answer = mid;
+        }else
+            l = mid +1;
+        
     }
+    
+    
+    
     return answer;
 }
