04bf1187638a9928ff0af4ec2c5a94b3a5d53dd4
Sung Ju Hong / 홍성주 <84310301+jshs214@users.noreply.github.com>
2024-12-12 20:50:01 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2068 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lis[1004];
+int n, len = 0;;
+pair<int, int> ans[1004];
+stack<int>stk;
+
+int main() {
+	cin >> n;
+
+	fill(lis, lis + 1004, -1);
+
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+
+		auto lowerPos = lower_bound(lis, lis + len, num);
+		int _pos = (int)(lowerPos - lis);
+		if (*lowerPos == -1)
+			len++;
+		*lowerPos = num;
+
+
+		ans[i].first = _pos;
+		ans[i].second = num;
+	}
+
+	cout << len << '\n';
+
+	for (int i = n - 1; i >= 0; i--) {
+		if (ans[i].first == len - 1) {
+			stk.push(ans[i].second);
+			len--;
+		}
+	}
+
+	while (stk.size()) {
+		cout << stk.top() << " ";
+		stk.pop();
+	}
+		
+
+
+
+
+	return 0;
+}
