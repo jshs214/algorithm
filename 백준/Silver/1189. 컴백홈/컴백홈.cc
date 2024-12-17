fcc7faa890da3e274380b1746e4158ce988da94c
Sung Ju Hong / 홍성주 <84310301+jshs214@users.noreply.github.com>
2024-12-17 15:11:59 +0900

    [Silver I] Title: 컴백홈, Time: 0 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Silver/1189. 컴백홈/컴백홈.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int r, c, k;
+
+char a[10][10];
+int visited[10][10];
+
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, -1, 0, 1};
+
+int go(int y, int x) {
+	if (y == 0 && x == c - 1) {
+		if (k == visited[y][x]) return 1;
+		return 0;
+	}
+	int ret = 0;
+	for (int i = 0; i < 4; i++) {
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+		if (ny < 0 || nx < 0 || ny >= r || nx >= c || visited[ny][nx] || a[ny][nx] == 'T') continue;
+		visited[ny][nx] = visited[y][x] + 1;
+
+		ret += go(ny, nx);
+		visited[ny][nx] = 0;
+	}
+	return ret;
+}
+
+int main() {
+	cin >> r >> c >> k;
+	
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			cin >> a[i][j];
+		}
+	}
+
+	visited[r - 1][0] = 1;
+
+	cout << go(r - 1, 0) <<"\n";
+
+
+	return 0;
+}
