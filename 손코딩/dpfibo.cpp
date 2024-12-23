b76abb2383f53dd5749f46f7500a9eb66e076333
labtob <jshs214@naver.com>
2024-12-23 13:41:20 +0900

    손코딩 문제

---
+++ 손코딩/dpfibo.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[104];
+
+int fibo(int n) {
+	if (n <= 0)
+		return 0;
+	if (n == 1)
+		return 1;
+
+	if (a[n])
+		return a[n];
+	return fibo(n-1) + fibo(n - 2);
+
+}
+int main() {
+
+	cout << fibo(6) ;
+
+	
+	return 0;
+}
--- 손코딩/insertsort.cpp
+++ 손코딩/insertsort.cpp
@@ -8,13 +8,14 @@ int main() {
 	for (int i = 1; i < n; i++) {
 		int tmp = a[i];
 		for (int j = i - 1; j >= 0; j--) {
-			if (a[j] < tmp) {
+			if (a[j] > tmp) {
 				a[j + 1] = a[j];
 				a[j] = tmp;
 			}
 			else break;
 		}
 	}
+
 
 
 	for (int i = 0; i < n; i++)
