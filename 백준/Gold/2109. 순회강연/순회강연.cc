da293e115b2e5b2a71bddf49a3a9859b26fe4c13
Sung Ju Hong / 홍성주 <84310301+jshs214@users.noreply.github.com>
2025-07-14 22:42:44 +0900

    [Gold III] Title: 순회강연, Time: 8 ms, Memory: 2324 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.cc
+++ 백준/Gold/2109. 순회강연/순회강연.cc
@@ -1,37 +1,37 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int n;
 vector<pair<int, int>> v;
 
-int ret = 0;
-
-//priority_queue 내림차순 (default param (type, container, sort))
-priority_queue<int, vector<int>, greater<int>> pq;
+int n, p, d, ret = 0;
+priority_queue< int, vector<int>, greater<int> > pq;
 
 int main() {
 	cin >> n;
 
-	for (int i = 0; i < n; i++) {
-		int p, d;
+	for (int i = 0; i < n; i ++)
+	{
 		cin >> p >> d;
 		v.push_back({ d, p });
 	}
-
 	sort(v.begin(), v.end());
-	
-	for (int i = 0; i < n; i++) {
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		ret += v[i].second;
 		pq.push(v[i].second);
 
 		if (pq.size() > v[i].first)
+		{
+			ret -= pq.top();
 			pq.pop();
-	}
+		}
 
-	while (pq.size()) {
-		ret += pq.top();
-		pq.pop();
 	}
 	cout << ret;
+
+
 
 	return 0;
 }
